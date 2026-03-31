66c3d6e909cf549f1a682c4ac855cb993b584e9d
Youssef-Hesham12 <heshamyoussef346@gmail.com>
2026-03-31 16:32:00 +0200

    hello

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 const protectedRoutes = ["/cart", "/checkout", "/allorders", "/profile"];
 const authRoutes = ["/login", "/register"];
-
+// بر
 export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const token = await getToken({
